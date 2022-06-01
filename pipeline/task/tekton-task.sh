54487346e2b3f6a08d73419713c4b82adefd2eeb
Yukihiro <Yukihiro.Azuma1@ibm.com>
2022-06-01 11:35:06 +0900

    test pipeline task

--- pipeline/task/tekton-task.sh
+++ pipeline/task/tekton-task.sh
@@ -1,3 +1,4 @@
 oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.5/git-clone.yaml
 oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/docker-build/0.1/docker-build.yaml
 oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildah/0.3/buildah.yaml
+oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/pytest/0.1/pytest.yaml
